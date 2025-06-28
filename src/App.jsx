75a66520fcb36187166c978b6c91d444e7df765d
Shipul <83777465+justdoit254@users.noreply.github.com>
2025-06-28 22:38:37 +0530

    Firebase and tostify setup (#2)
    * firebase and toastify setup
    
    * signup implementation
    
    * OAuth signup
    
    * signin implementation
    
    * forgot password implementation

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
@@ -21,6 +22,18 @@ function App() {
           <Route path="/offers" element={<Offers />} />
         </Routes>
       </BrowserRouter>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
   );
 }
--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react-toastify";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
@@ -9,11 +11,24 @@ const ForgotPassword = () => {
     setEmail(e.target.value);
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+    } catch (error) {
+      console.log("Reset password error", error);
+      toast("Reset password error");
+    }
+  };
+
   return (
     <section>
       <h1 className="text-3xl text-center mb-6 font-bold">Forgot password</h1>
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
-        <div className="md:w-[67%] lg:w-[50%]">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 lg:mb-0">
           <img
             src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8a2V5c3xlbnwwfHwwfHx8MA%3D%3D"
             alt="key"
@@ -21,7 +36,7 @@ const ForgotPassword = () => {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               id="email"
