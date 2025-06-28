d78b7bf39c7c467715b97212a397d03c55b50db0
Shipul <shipul2910@gmail.com>
2025-06-28 22:34:03 +0530

    OAuth signup

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import { db } from "../firebase";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
 
 const OAuth = () => {
+  const navigate = useNavigate();
+
+  const onGoogleClick = async (e) => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      //Check for the user
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user?.displayName,
+          email: user?.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      toast.success("Welcome to realtor");
+      navigate("/");
+    } catch (error) {
+      toast("Google auth error");
+      console.log("Google auth error", error);
+    }
+  };
+
   return (
     <button
-      type="submit"
+      type="button"
+      onClick={onGoogleClick}
       className="flex justify-center items-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium shadow-md hover:bg-red-800 active:bg-red-900 hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out"
     >
       <FcGoogle className="text-2xl bg-white rounded-full mr-2" /> Continue with
