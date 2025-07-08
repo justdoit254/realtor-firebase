ab24979bcfcbf8db7525b511ccabe34eddb5ae23
Shipul <shipul2910@gmail.com>
2025-07-08 22:52:03 +0530

    update profile name

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Profile = () => {
   const auth = getAuth();
   const navigate = useNavigate();
+  console.log("auth", auth);
 
+  const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -15,6 +20,34 @@ const Profile = () => {
   const onLogOut = () => {
     auth.signOut();
     navigate("/");
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async () => {
+    try {
+      //update display name in firebase auth
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, { displayName: name });
+      }
+
+      //update name in the firestore
+      const docRef = doc(db, "users", auth.currentUser.uid);
+      await updateDoc(docRef, { name }); //Make name a compulsory field in sign-in
+      // auth.currentUser.displayName
+      //   ? await updateDoc(docRef, { name })
+      //   : await setDoc(docRef, { name });
+
+      toast.success("Profile details update");
+    } catch (error) {
+      console.error(error);
+      toast.error("Could not update profile details");
+    }
   };
 
   return (
@@ -27,8 +60,11 @@ const Profile = () => {
               type="text"
               id="name"
               value={name}
-              disabled
-              className="w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              }`}
             />
             <input
               type="email"
@@ -40,8 +76,14 @@ const Profile = () => {
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
               <p className="flex items-center">
                 Do you want to change your name?
-                <span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">
-                  Edit
+                <span
+                  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                >
+                  {changeDetail ? "Apply change" : "Edit"}
                 </span>
               </p>
               <p
