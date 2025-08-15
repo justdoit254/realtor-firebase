b548dd0768b542d526263488be9cac27a37ec559
Shipul <shipul2910@gmail.com>
2025-08-15 18:20:05 +0530

    environment variables added

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "./config";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyA6qjIi6F5rWnNvP0rb7dZL_5_7zSjSs5s",
-    authDomain: "realtor-firebase-78760.firebaseapp.com",
-    projectId: "realtor-firebase-78760",
-    storageBucket: "realtor-firebase-78760.firebasestorage.app",
-    messagingSenderId: "249262569314",
-    appId: "1:249262569314:web:ee17fc8d42c35967d25e3b"
-};
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
